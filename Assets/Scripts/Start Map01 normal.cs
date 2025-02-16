5d903eee2e078512e910353812d9629a814811c9
Daniel Binar <daniel@srv.g-srv.eu>
2025-02-16 17:16:30 +0100

    první část
    Lze spusit hru na mid obtížnost

---
+++ Assets/Scripts/Start Map01 normal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class map0Normal : MonoBehaviour
+{
+    public void RunMediumDif()
+    {
+        SceneManager.LoadScene("Map01 - Normal");
+    }
+}
---
+++ Assets/Scripts/map01Selector.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class map01Selector : MonoBehaviour
+{
+    public void OpenDificultSelector()
+    {
+        SceneManager.LoadScene("Map01 dificult selector");
+    }
+}
